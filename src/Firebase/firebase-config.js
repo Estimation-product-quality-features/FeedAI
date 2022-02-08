78283a791a22b1e4c0b96907c3a846122015f863
Hannes <ha_ge@uni-bremen.de>
2022-02-08 15:33:30 +0100

    firebase related changes, its now integrated in the docker image

--- src/Firebase/firebase-config.js
+++ src/Firebase/firebase-config.js
@@ -4,9 +4,9 @@ import "firebase/storage";
 
 const app = firebase.initializeApp( {
     apiKey: "AIzaSyDYg_0mSiDhneXuruEKmpNyn3f-AcSgh_I",
-    authDomain: "eisauge-877b5.firebaseapp.com",
-    projectId: "eisauge-877b5",
-    storageBucket: "eisauge-877b5.appspot.com",
+    authDomain: "tracker-2baa0.firebaseapp.com",
+    projectId: "tracker-2baa0",
+    storageBucket: "tracker-2baa0.appspot.com",
     messagingSenderId: "499483603485",
     appId: "1:499483603485:web:25e46770acf6b11e27ccf4",
     measurementId: "G-B7YK1NPRC6"
